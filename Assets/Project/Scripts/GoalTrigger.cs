ec58580e0e3fe6fb7e6f17d648d6546662fe3d48
Taro Omiya <taro.omiya@omiyagames.com>
2019-10-06 15:50:25 -0400

    Adding a working goal post.  Placeholder graphic should be replaced ASAP.

---
+++ Assets/Project/Scripts/GoalTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class GoalTrigger : MonoBehaviour
+{
+    [SerializeField]
+    string detectTag = "Player";
+    [SerializeField]
+    float delaySwitchSceneSeconds = 0.5f;
+
+    bool isTriggered = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if((collision.CompareTag(detectTag)) && (isTriggered == false))
+        {
+            StartCoroutine(SwitchScene());
+            isTriggered = true;
+        }
+    }
+
+    IEnumerator SwitchScene()
+    {
+        yield return new WaitForSeconds(delaySwitchSceneSeconds);
+        Debug.Log("Changing scene!");
+        OmiyaGames.Singleton.Get<OmiyaGames.Scenes.SceneTransitionManager>().LoadNextLevel();
+    }
+}

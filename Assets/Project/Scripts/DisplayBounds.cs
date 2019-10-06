665b96a37d3cdb4952101f76eb523a2e2fce3ab5
Taro Omiya <taro.omiya@omiyagames.com>
2019-10-06 12:07:38 -0400

    Working on polishing the toggle activation script a bit to handle more stranger cases.

--- Assets/Project/Scripts/DisplayBounds.cs
+++ Assets/Project/Scripts/DisplayBounds.cs
@@ -5,11 +5,25 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]
 public class DisplayBounds : MonoBehaviour
 {
+    [SerializeField]
+    WindowRect rect;
+
+    public int Priority
+    {
+        get => rect.transform.GetSiblingIndex();
+    }
+
+    public ProceduralSpriteGenerator.WindowLayer Layer
+    {
+        get => rect.DisplayLayer;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         ToggleActivation toggle = collision.GetComponent<ToggleActivation>();
         if(toggle != null)
         {
+            gameObject.layer = 1;
             toggle.EnterBounds(this);
         }
     }

f5198e87bf8f1dd6c9929a3ed018510534f6fb40
Taro Omiya <taro.omiya@omiyagames.com>
2019-10-06 00:33:47 -0400

    Drastically reducing the work by simply having render textures.

--- Assets/Project/Scripts/SpriteMaskEditor.cs
+++ Assets/Project/Scripts/SpriteMaskEditor.cs
@@ -16,16 +16,14 @@ public class SpriteMaskEditor : MonoBehaviour
         public bool IsDirty { get; set; } = true;
 
         // FIXME: get rid of the getter
-        public Rect ViewportRect { get => Window.Display.uvRect;  set => Window.Display.uvRect = value; }
+        public Rect ViewportRect { set => Window.Display.uvRect = value; }
     }
 
     [SerializeField]
     Camera orthogonalCamera;
 
-    bool isDirty = true;
     List<WindowInfo> allWindows = null;
     Dictionary<DragDrop, WindowInfo> dragToWindowMap = null;
-    readonly HashSet<ProceduralSpriteGenerator.WindowLayer> dirtyLayers = new HashSet<ProceduralSpriteGenerator.WindowLayer>();
 
     #region Properties
     /// <summary>
@@ -59,108 +57,50 @@ public class SpriteMaskEditor : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // FIXME: figure out which render texture to release resources from
+        HashSet<ProceduralSpriteGenerator.WindowLayer> activeLayers = new HashSet<ProceduralSpriteGenerator.WindowLayer>();
+
         // Calculate the screen corners
         foreach (Transform child in transform)
         {
             WindowRect script = child.GetComponent<WindowRect>();
             if (script != null)
             {
+                // Setup the window wrapper
                 WindowInfo newInfo = new WindowInfo(script);
                 AllWindows.Add(newInfo);
+
+                // Bind to all the windows events
                 DragToWindowMap.Add(script.DragScript, newInfo);
                 script.DragScript.OnBeforeDrag += MarkDirty;
 
-                ProceduralSpriteGenerator.SpriteData data = ProceduralSpriteGenerator.GetSprite(script.DisplayLayer);
-                script.Display.texture = data.CameraTexture;
+                // Apply the texture to the window's display
+                script.Display.texture = ProceduralSpriteGenerator.GetTexture(script.DisplayLayer);
+
+                // Add this window's layer into the active set
+                activeLayers.Add(script.DisplayLayer);
             }
         }
-    }
 
-    private void MarkDirty(DragDrop source, Vector2 from, Vector2 to)
-    {
-        if (DragToWindowMap.TryGetValue(source, out WindowInfo info) == true)
+        // Go through all layers
+        ProceduralSpriteGenerator.WindowLayer layer;
+        for (int layerId = 0; layerId < ProceduralSpriteGenerator.NumberOfLayers; ++layerId)
         {
-            // Mark the corresponding window dirty
-            info.IsDirty = true;
-            isDirty = true;
+            layer = (ProceduralSpriteGenerator.WindowLayer)layerId;
+            if (activeLayers.Contains(layer) == false)
+            {
+                // Release the unused textures
+                ProceduralSpriteGenerator.GetTexture(layer).Release();
+            }
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isDirty)
-        {
-            // Update all the windows
-            UpdateAllWindows();
-
-            // Draw the sprite
-            //foreach (ProceduralSpriteGenerator.WindowLayer layer in dirtyLayers)
-            //{
-            //    ProceduralSpriteGenerator.SpriteData data = ProceduralSpriteGenerator.GetSprite(layer);
-
-            //    // Go through all coordinates
-            //    for (int i = 0; i < data.Count; ++i)
-            //    {
-            //        if (IsPixelOpaque(layer, data, i))
-            //        {
-            //            data[i] = 255;
-            //        }
-            //        else
-            //        {
-            //            data[i] = 0;
-            //        }
-            //    }
-
-            //    // Apply all the changes
-            //    data.Apply();
-            //}
-
-            // Re-calculate each window's rect
-            isDirty = false;
-        }
-    }
-
-    private bool IsPixelOpaque(ProceduralSpriteGenerator.WindowLayer layer, ProceduralSpriteGenerator.SpriteData data, int pixelIndex)
-    {
-        // Convert the index to coordinates
-        Vector2Int pixelPosition = new Vector2Int(Mathf.FloorToInt(pixelIndex % data.Width), Mathf.FloorToInt(pixelIndex / data.Width));
-
-        // Go through all the windows in reverse order
-        bool isPixelOpaque = false;
-        for (int index = (AllWindows.Count - 1); index >= 0; --index)
-        {
-            // Check if this window contains this pixel
-            if (AllWindows[index].ViewportRect.Contains(pixelPosition))
-            {
-                // If so, check the window's layer
-                if (layer == ProceduralSpriteGenerator.WindowLayer.None)
-                {
-                    // If it's the default layer, it should always be drawn
-                    isPixelOpaque = true;
-                }
-                else
-                {
-                    // If on a special layer, see if this window exposes this layer
-                    // The top-most window (i.e. last) should have the highest priority
-                    isPixelOpaque = ((AllWindows[index].Window.DisplayLayer & layer) != 0);
-                }
-                break;
-            }
-        }
-
-        return isPixelOpaque;
-    }
-
-    private void UpdateAllWindows()
-    {
         // Setup some variables
         Rect updateRect = new Rect();
         Vector3 bottomLeftViewportPosition, topRightViewportPosition;
-
-        // Reset the layer list
-        dirtyLayers.Clear();
-        dirtyLayers.Add(ProceduralSpriteGenerator.WindowLayer.None);
 
         // Go through all windows
         foreach (WindowInfo info in AllWindows)
@@ -179,22 +119,18 @@ public class SpriteMaskEditor : MonoBehaviour
                 updateRect.yMax = topRightViewportPosition.y;
                 info.ViewportRect = updateRect;
 
-                // Check which layer needs to be added
-                AddLayer(info, ProceduralSpriteGenerator.WindowLayer.WaterOnly, dirtyLayers);
-                AddLayer(info, ProceduralSpriteGenerator.WindowLayer.WindOnly, dirtyLayers);
-
                 // Mark this window as clean
                 info.IsDirty = false;
             }
         }
     }
 
-    private static void AddLayer(WindowInfo info, ProceduralSpriteGenerator.WindowLayer layerToCheck, HashSet<ProceduralSpriteGenerator.WindowLayer> dirtyLayers)
+    private void MarkDirty(DragDrop source, Vector2 from, Vector2 to)
     {
-        if (((info.Window.DisplayLayer & layerToCheck) != 0) && (dirtyLayers.Contains(layerToCheck) == false))
+        if (DragToWindowMap.TryGetValue(source, out WindowInfo info) == true)
         {
-            // Add the layer into the list
-            dirtyLayers.Add(layerToCheck);
+            // Mark the corresponding window dirty
+            info.IsDirty = true;
         }
     }
 }

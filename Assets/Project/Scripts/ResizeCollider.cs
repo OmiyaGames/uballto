8e3bd00526bf49d4ad967ff9905aae4f1623983a
Taro Omiya <taro.omiya@omiyagames.com>
2019-10-07 14:41:18 -0400

    Adding movable blocks.

---
+++ Assets/Project/Scripts/ResizeCollider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ResizeCollider : MonoBehaviour
+{
+    [SerializeField]
+    RectTransform rectTransform;
+    [SerializeField]
+    BoxCollider2D boxCollider;
+
+#if UNITY_EDITOR
+    void Update()
+    {
+        if (rectTransform && boxCollider)
+        {
+            boxCollider.size = new Vector2(rectTransform.rect.width, rectTransform.rect.height);
+        }
+    }
+#endif
+}

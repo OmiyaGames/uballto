24339967310cab92344aafd2fa1ceb3d5377b753
Taro Omiya <taro.omiya@omiyagames.com>
2019-10-09 19:44:35 -0400

    Making first effort at normalizing the game elements to the width of the screen.  Currently doesn't work.

---
+++ Assets/Project/Scripts/Screen Resize/NormalizeCamera.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class NormalizeCamera : MonoBehaviour
+{
+    [SerializeField]
+    float expectedScreenResolution = 16f / 9f;
+
+    Camera cameraCache;
+    float lastScreenResolution = -1f, currentScreenResolution;
+
+    public Camera Camera
+    {
+        get => OmiyaGames.Utility.GetComponentCached(this, ref cameraCache);
+    }
+
+    public static void GetCurrentScreenResolution(out float currentResolution)
+    {
+        currentResolution = Screen.width;
+        currentResolution /= Screen.height;
+    }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        // Get current screen resolution
+        GetCurrentScreenResolution(out lastScreenResolution);
+        UpdateResolution(lastScreenResolution);
+    }
+
+    private void Update()
+    {
+        // Get current screen resolution
+        GetCurrentScreenResolution(out currentScreenResolution);
+        if (Mathf.Approximately(currentScreenResolution, lastScreenResolution))
+        {
+            UpdateResolution(currentScreenResolution);
+            lastScreenResolution = currentScreenResolution;
+        }
+    }
+
+    private void UpdateResolution(float currentResolution)
+    {
+        // Grab camera
+        float cameraOrthoSize = Camera.orthographicSize;
+
+        // Increase the orthographic size so the width of the screen is fit.
+        float lostScreenWidth = expectedScreenResolution - currentResolution;
+        Camera.orthographicSize = cameraOrthoSize * (1 + lostScreenWidth);
+    }
+}

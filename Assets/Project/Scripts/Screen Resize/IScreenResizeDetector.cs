795885312bf37711f7b86dbadeac26eda31aa30d
Taro Omiya <taro.omiya@omiyagames.com>
2019-10-09 22:01:00 -0400

    Implementing IScreenResizeDetector properly.

--- Assets/Project/Scripts/Screen Resize/IScreenResizeDetector.cs
+++ Assets/Project/Scripts/Screen Resize/IScreenResizeDetector.cs
@@ -2,17 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IScreenResizeDetector : MonoBehaviour
+public abstract class IScreenResizeDetector : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Vector2Int lastScreenDimensions = Vector2Int.zero;
+    float lastScreenResolution = 0f;
+
+    public static void GetCurrentScreenResolution(out float currentResolution)
     {
-        
+        currentResolution = Screen.width;
+        currentResolution /= Screen.height;
     }
 
-    // Update is called once per frame
-    void Update()
+    public abstract void OnScreenSizeChanged(int lastScreenWidth, int lastScreenHeight, float lastScreenResolution);
+
+    // Start is called before the first frame update
+    public virtual void Awake()
     {
-        
+        // Update cache values
+        lastScreenDimensions.x = Screen.width;
+        lastScreenDimensions.y = Screen.height;
+        GetCurrentScreenResolution(out lastScreenResolution);
+
+        // Run the event
+        OnScreenSizeChanged(lastScreenDimensions.x, lastScreenDimensions.y, lastScreenResolution);
+    }
+
+    public virtual void Update()
+    {
+        if ((Screen.width != lastScreenDimensions.x) || (Screen.height != lastScreenDimensions.y))
+        {
+            // Run the event
+            OnScreenSizeChanged(lastScreenDimensions.x, lastScreenDimensions.y, lastScreenResolution);
+
+            // Update cache values
+            lastScreenDimensions.x = Screen.width;
+            lastScreenDimensions.y = Screen.height;
+            GetCurrentScreenResolution(out lastScreenResolution);
+        }
     }
 }

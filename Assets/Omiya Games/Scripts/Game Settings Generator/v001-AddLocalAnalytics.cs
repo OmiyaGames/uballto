488dae5bff67e76be90afae40b7c47973b773b62
Taro Omiya <taro.omiya@omiyagames.com>
2019-10-09 21:58:53 -0400

    Fixing the number of levels unlocked to 9

--- Assets/Omiya Games/Scripts/Game Settings Generator/v001-AddLocalAnalytics.cs
+++ Assets/Omiya Games/Scripts/Game Settings Generator/v001-AddLocalAnalytics.cs
@@ -35,7 +35,7 @@
     public class AddLocalAnalytics : SettingsVersionGeneratorDecorator
     {
         public const ushort AppVersion = 1;
-        public const int DefaultNumLevelsUnlocked = 8;
+        public const int DefaultNumLevelsUnlocked = 9;
         public const string DefaultNumLevelsUnlockedName = "DefaultNumLevelsUnlocked";
 
         public override ushort Version

22f6a29f8da464fd6f2fa1fa16a8ba2b2aedfa0c
Taro Omiya <taro.omiya@omiyagames.com>
2019-10-09 22:02:07 -0400

    Updating NormalizeCamera to use IScreenResizeDetector to change camera field of view size.

--- Assets/Project/Scripts/Screen Resize/NormalizeCamera.cs
+++ Assets/Project/Scripts/Screen Resize/NormalizeCamera.cs
@@ -3,51 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Camera))]
-public class NormalizeCamera : MonoBehaviour
+public class NormalizeCamera : IScreenResizeDetector
 {
     [SerializeField]
     float expectedScreenResolution = 16f / 9f;
 
     Camera cameraCache;
-    float lastScreenResolution = -1f, currentScreenResolution;
+    float currentScreenResolution;
 
     public Camera Camera
     {
         get => OmiyaGames.Utility.GetComponentCached(this, ref cameraCache);
     }
 
-    public static void GetCurrentScreenResolution(out float currentResolution)
-    {
-        currentResolution = Screen.width;
-        currentResolution /= Screen.height;
-    }
-
-    // Start is called before the first frame update
-    void Awake()
-    {
-        // Get current screen resolution
-        GetCurrentScreenResolution(out lastScreenResolution);
-        UpdateResolution(lastScreenResolution);
-    }
-
-    private void Update()
-    {
-        // Get current screen resolution
-        GetCurrentScreenResolution(out currentScreenResolution);
-        if (Mathf.Approximately(currentScreenResolution, lastScreenResolution))
-        {
-            UpdateResolution(currentScreenResolution);
-            lastScreenResolution = currentScreenResolution;
-        }
-    }
-
-    private void UpdateResolution(float currentResolution)
+    public override void OnScreenSizeChanged(int lastScreenWidth, int lastScreenHeight, float lastScreenResolution)
     {
         // Grab camera
         float cameraOrthoSize = Camera.orthographicSize;
 
+        // Get the current screen resolution
+        GetCurrentScreenResolution(out float currentScreenResolution);
+
         // Increase the orthographic size so the width of the screen is fit.
-        float lostScreenWidth = expectedScreenResolution - currentResolution;
+        float lostScreenWidth = expectedScreenResolution - currentScreenResolution;
         Camera.orthographicSize = cameraOrthoSize * (1 + lostScreenWidth);
     }
 }
